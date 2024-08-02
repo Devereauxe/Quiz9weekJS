bad36e28f78146a339affee2d9b62b4638fb638b
Devereauxe <160384884+Devereauxe@users.noreply.github.com>
2024-08-02 14:51:35 +0500

    finished

---
+++ script.js
@@ -0,0 +1,32 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', result);
+
+const par = document.querySelector('#show');
+
+function result(e) {
+    e.preventDefault();
+
+    let points = 0;
+
+    if (document.querySelector('#answer1').checked) {
+        points++;
+    }
+
+    if (document.querySelector('#answer2').checked) {
+        points++;
+    }
+
+    if (document.querySelector('#answer3').checked) {
+        points++;
+    }
+
+    if (document.querySelector('#answer4').checked) {
+        points++;
+    }
+
+    if (document.querySelector('#answer5').checked) {
+        points++;
+    }
+    par.textContent = "Ваши баллы:" + points;
+}
+
